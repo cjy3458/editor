9ab6a618119044b35de30836c0c2dbdb50f0d673
CJY <cjy3458@naver.com>
2023-09-24 23:05:23 +0900

    리액트 에디터 라이브러리 연습

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "@toast-ui/editor/dist/toastui-editor.css";
+import { Editor } from "@toast-ui/react-editor";
+
+const MyComponent = () => (
+  <Editor
+    initialValue="hello react editor world!"
+    previewStyle="vertical"
+    height="600px"
+    initialEditType="markdown"
+    useCommandShortcut={true}
+  />
+);
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <MyComponent />
       </header>
     </div>
   );
